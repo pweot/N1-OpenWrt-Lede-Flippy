8b9763843e5d762360dd9827f296fc8b8708c6c7
pweot <pweot@126.com>
2023-09-16 00:52:00 +0800

    Update src.sh

--- src.sh
+++ src.sh
@@ -2,6 +2,6 @@
 cd openwrt
 # Add a feed source
 echo 'src-git SSRplus https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 #echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
