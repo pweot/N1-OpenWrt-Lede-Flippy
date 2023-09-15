8dd80c6d6b34cbdbd9041052590f2dece7ca7c00
pweot <pweot@126.com>
2023-09-16 00:52:29 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,7 +13,7 @@ git clone https://github.com/kwxos/open-page.git package/lean/
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/lean/luci-app-amlogic
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/lean/luci-app-openclash
 #Add luci-app-passwall
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/lean/luci-app-passwall
+#git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/lean/luci-app-passwall
 #Add luci-app-smartdns
 #git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
 #Add luci-app-adguardhome
